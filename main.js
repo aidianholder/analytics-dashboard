2375c071e742edcb77d91fb742f687a9f1dee53c
Aidian Holder <aidianholder@gmail.com>
2017-03-14 20:33:30 -0700

    cleaned up chart labels, gridlines & added "article" filter to story traffic

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"use strict";
+        'use strict';
         gapi.analytics.ready(function() {
             var dashNS = {
                 userFilter: 'none',
@@ -13,10 +13,10 @@
 
             $( '#reporterFilter' ).on('change', function(){
                 dashNS.userFilter = $("#reporterFilter").val();
-                fireDaily(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
-                fireRefers(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
-                fireHourly(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
-                fireMonthly(dashNS.userFilter);
+                articleTraffic(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                referalDetail(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                trafficByDayHour(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                thirtyDayTotals(dashNS.userFilter);
               });
 
             var dateRangeSelector = new gapi.analytics.ext.DateRangeSelector({
@@ -27,12 +27,12 @@
 
             dateRangeSelector.on('change', function(data){
                 dashNS.dateRange = data;
-                fireDaily(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
-                fireRefers(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
-                fireHourly(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                articleTraffic(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                referalDetail(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                trafficByDayHour(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
             });
 
-              function fireDaily(dateStart, dateEnd, reporter, storyHed){
+              function articleTraffic(dateStart, dateEnd, reporter, storyHed){
                   $('#todayTraffic').html(loadingIcon);
 
                   var rawTraffic = new gapi.analytics.report.Data({
@@ -47,12 +47,12 @@
                   });
 
                   if (reporter != 'none'){
-                      rawTraffic.set({query: {filters: 'ga:dimension1=@' + reporter}})
-                  };
+                      rawTraffic.set({query: {filters: 'ga:dimension1=@' + reporter}});
+                  }
 
                   if (typeof storyHed != 'undefined'){
-                      rawTraffic.set({query: {filters: 'ga:dimension2=@' + storyHed}})
-                  };
+                      rawTraffic.set({query: {filters: 'ga:dimension2=@' + storyHed}});
+                  }
 
                   rawTraffic.on('success', function(response){
                           /*$("#go-button").toggleClass('disabled');*/
@@ -61,19 +61,19 @@
                           totals = response.totalsForAllResults;
                           u = parseInt(totals['ga:uniquePageviews']).toLocaleString();
                           p = parseInt(totals['ga:pageviews']).toLocaleString();
-                          t = totals['ga:avgTimeOnPage']
-                          s = toHHMMSS(t)
+                          t = totals['ga:avgTimeOnPage'];
+                          s = toHHMMSS(t);
 
-                          $( '#totalUnique' ).html(u)
-                          $( '#totalPages').html(p)
-                          $( '#totalAveTime').html(s)
+                          $( '#totalUnique' ).html(u);
+                          $( '#totalPages').html(p);
+                          $( '#totalAveTime').html(s);
 
-                          stories = []
-                          tales = response.rows.slice(0,15)
+                          stories = [];
+                          tales = response.rows.slice(0,15);
                           tales.forEach(function(row){
-                              article = {title:row[0], uniques:row[1], pageviews:row[2], avetime: toHHMMSS(row[3])}
-                              stories.push(article)
-                          })
+                              article = {title:row[0], uniques:row[1], pageviews:row[2], avetime: toHHMMSS(row[3])};
+                              stories.push(article);
+                          });
 
                           var source = $("#daily-table").html();
                           var template = Handlebars.compile(source);
@@ -83,32 +83,32 @@
                           /*var heds = $('#todayTraffic th.hed')*/
                           if (storyHed === undefined){
                               $('#todayTraffic tbody').one( "click", "th.hed", function(){
-                                    storyHed = $( this ).text()
-                                    fireDaily(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], reporter, storyHed)
-                                    fireRefers(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], reporter, storyHed)
-                                    fireHourly(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], reporter, storyHed)
+                                    storyHed = $( this ).text();
+                                    articleTraffic(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], reporter, storyHed);
+                                    referalDetail(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], reporter, storyHed);
+                                    trafficByDayHour(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], reporter, storyHed);
                                     /*$("#go-button").toggleClass('disabled');*/
-                              })
+                              });
                           } else {
                             $('#todayTraffic tbody').on("click", "th.hed", function(){
-                                        storyHed = undefined
-                                      fireDaily(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
-                                      fireRefers(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
-                                      fireHourly(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                                      storyHed = undefined;
+                                      articleTraffic(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                                      referalDetail(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                                      trafficByDayHour(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
                                       /*$("#go-button").toggleClass('disabled');*/
 
-                                    })
+                                    });
                               }
-                      })
+                      });
 
                   rawTraffic.execute();
               }
 
-              function fireRefers(dateStart, dateEnd, reporter, storyHed){
+              function referalDetail(dateStart, dateEnd, reporter, storyHed){
 
                   var dailyRefer, referalSources, refer;
-                  $('#todayReferral').html(loadingIcon)
-                  var dailyRefer = new gapi.analytics.report.Data({
+                  $('#todayReferral').html(loadingIcon);
+                  dailyRefer = new gapi.analytics.report.Data({
 
                           query: {
                                   ids: 'ga:60921314',
@@ -119,37 +119,37 @@
                                   sort: '-ga:uniquePageviews',
                                   'max-results': '10'
                           }
-                      })
+                      });
 
                   if (reporter != 'none'){
-                      dailyRefer.set({query: {filters: 'ga:dimension1=@' + reporter}})
+                      dailyRefer.set({query: {filters: 'ga:dimension1=@' + reporter}});
                   }
 
 
                   if (typeof storyHed != 'undefined'){
-                      dailyRefer.set({query: {filters: 'ga:dimension2=@' + storyHed}})
+                      dailyRefer.set({query: {filters: 'ga:dimension2=@' + storyHed}});
                   }
 
                   dailyRefer.on('success', function(response){
-                      console.log(response)
-                      referalSources = []
+                      /*console.log(response);*/
+                      referalSources = [];
                       response.rows.forEach(function(row){
-                          refer = {site:row[0], uniques:row[1], pageviews:row[2]}
-                          console.log(refer)
-                          referalSources.push(refer)
-                      })
+                          refer = {site:row[0], uniques:row[1], pageviews:row[2]};
+                          /*console.log(refer)*/
+                          referalSources.push(refer);
+                      });
 
                       var source = $( "#referral-table").html();
                       var template = Handlebars.compile(source);
                       var html = template(referalSources);
-                      $( '#todayReferral' ).html(html)
-                  })
+                      $( '#todayReferral' ).html(html);
+                  });
 
 
                   dailyRefer.execute();
-              };
+              }
 
-              function fireHourly(dateStart, dateEnd, reporter, storyHed){
+              function trafficByDayHour(dateStart, dateEnd, reporter, storyHed){
                   var s, e, sd, ed, d, hourlyMetrics;
                   hourlyMetrics = new gapi.analytics.googleCharts.DataChart({
                       query: {
@@ -166,33 +166,39 @@
                           type: 'COLUMN',
                           options: {
                               width: '80%',
+                              chartArea: {left:20, top:10, width: '80%', height: '80%'},
                               hAxis: {
                                 format: 'MMM d h',
+                                gridlines: {
+                                  count: -1
+                                }
+                              },
+                              vAxis: {
+                                format: 'decimal',
                                 gridlines: {
                                   count: -1
                                 }
                               }
                           }
                       }
-                  })
+                  });
 
                   if (reporter != 'none'){
-                      hourlyMetrics.set({query: {filters: 'ga:dimension1=@' + reporter}})
+                      hourlyMetrics.set({query: {filters: 'ga:dimension1=@' + reporter}});
                   }
 
                   if (typeof storyHed != 'undefined'){
-                      hourlyMetrics.set({query: {filters: 'ga:dimension2=@' + storyHed}})
+                      hourlyMetrics.set({query: {filters: 'ga:dimension2=@' + storyHed}});
                   }
 
-                  s = $('div.DateRangeSelector div.DateRangeSelector-item:last-child input').attr('min')
-                  e = $('div.DateRangeSelector div.DateRangeSelector-item:first-child input').attr('max')
-                  sd = new Date(s)
-                  ed = new Date(e)
-                  d = ed - sd
+                  s = $('div.DateRangeSelector div.DateRangeSelector-item:last-child input').attr('min');
+                  e = $('div.DateRangeSelector div.DateRangeSelector-item:first-child input').attr('max');
+                  sd = new Date(s);
+                  ed = new Date(e);
+                  d = ed - sd;
 
                   if (d > 86400000){
                       hourlyMetrics.set({query: {dimensions: 'ga:day'}});
-                      
                   } else {
                       hourlyMetrics.set({query: {dimensions: 'ga:dateHour'}});
                       hourlyMetrics.set({chart: {options: {hAxis:{format: 'd hh'}}}});
@@ -203,8 +209,8 @@
                   hourlyMetrics.on('success', function(response){
                       console.log(response.chart);
                       console.log(response.data);
-                    })
-              };
+                    });
+              }
 
 
 
@@ -213,23 +219,24 @@
                 var sortMonthMetric = '-' + metricMonthPicked;
                 monthlyMetrics.set({query: {metrics: metricMonthPicked}});
                 monthlyMetrics.execute();
-            })
+            });
 
 
 
 
-          function fireMonthly(reporter, storyHed){
+          function thirtyDayTotals(reporter, storyHed){
               var monthlyMetrics = new gapi.analytics.googleCharts.DataChart({
                       query: {
                         ids: 'ga:60921314',
                           metrics: 'ga:uniquePageviews',
-                          dimensions: 'ga:date', 'start-date': '30daysAgo', 'end-date': 'today',
+                          dimensions: 'ga:date', 'start-date': '30daysAgo', 'end-date': 'today'
                       },
                       chart: {
                           container: 'monthlyChart',
                           type: 'COLUMN',
                           options: {
                               width: '80%',
+                              chartArea: {left:20, top:10, width: '80%', height: '80%'},
                               title: 'Unique Page Views',
                               hAxis: {
                                 format: 'MMM dd y',
@@ -242,20 +249,20 @@
                  });
 
               if (reporter != 'none'){
-                  monthlyMetrics.set({query: {filters: 'ga:dimension1=@' + reporter}})
-              };
+                  monthlyMetrics.set({query: {filters: 'ga:dimension1=@' + reporter}});
+              }
 
               if (typeof storyHed != 'undefined'){
-                      monthlyMetrics.set({query: {filters: 'ga:dimension2=@' + storyHed}})
-                  };
+                      monthlyMetrics.set({query: {filters: 'ga:dimension2=@' + storyHed}});
+                  }
 
               monthlyMetrics.execute();
-          };
+          }
 
-                  fireDaily(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
-                fireRefers(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
-                fireMonthly(dashNS.userFilter)
-                fireHourly(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                  articleTraffic(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                referalDetail(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
+                thirtyDayTotals(dashNS.userFilter)
+                trafficByDayHour(dashNS.dateRange['start-date'], dashNS.dateRange['end-date'], dashNS.userFilter);
 
         })/*end ready function for GAPI*/
 
